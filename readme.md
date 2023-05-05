fb84bc38aad05d7a006e5494f261911d19a2d865
Nejra <skandronejra@gmail.com>
2023-05-06 01:21:14 +0200

    Automated tests for login functionality

--- readme.md
+++ readme.md
@@ -1,26 +1,5 @@
-Homework:
+Task2:
 
-1.Create a maven tests project
-2.Add dependencies with the latest versions: TestNG, Selenium,
-WebDriverManager
-3.Create a test with before and after annotations that will open the browser
-on page https://demo.placelab.com and then print in terminal “Opened
-browser”
-4. Enable running a test in min 2 browsers
-5. When your test is being run, now print in the terminal: “Opened browser
-   ${browserName}” (should work for whatever browser your config
-   supports)
-6. Fetch a webpage title and print it out in the terminal
-7. Run your first automated test!
-8. Upload your project to GitHub (Project name: placelab-qamp-ui-tests;
-   Note: Create a Pull Request ready for review)
-
-Best deadline: May 3rd
-
-resources/testng.xml basic config
-
-How to setup a browser driver manually: https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/
-
-How to run a test:
-1. Make sure that you are executed in the directory where your pom.xml file is
-2. Run from terminal: mvn clean verify
+1. Automate 3-5 tests for login functionality (make a good use of
+   testNG annotations)
+2. Open a PR ready for review (on the existing repository)
---
+++ src/test/java/com/placelab/tests/NegativeLoginTestInvalidPassword.java
@@ -0,0 +1,63 @@
+package com.placelab.tests;
+
+import com.placelab.utils.WebDriverSetup;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class NegativeLoginTestInvalidPassword {
+    private WebDriver driver;
+    private final String browserName = System.getProperty("browser");
+
+    @Parameters("browser")
+    @BeforeTest
+    private void setDriverAndCheckIfLoginFormIsDisplayed() {
+        this.driver = WebDriverSetup.getWebDriver(browserName);
+        this.driver.get("https://demo.placelab.com");
+        final LoginFormDisplayed loginForm = new LoginFormDisplayed();
+        loginForm.isLoginFormDisplayed(this.driver);
+    }
+
+    @Parameters("email")
+    @Test
+    public void loginWithValidCredentials(final String email) {
+
+        driver.findElement(By.id("email")).sendKeys(email);
+        driver.findElement(By.id("password")).sendKeys("123987");
+        driver.findElement(By.cssSelector("#login_form > input.btn.large-btn[value = 'Log in']")).click();
+
+        final Duration timeout = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#login > div.row > div > div")));
+        final boolean isErrorMessageDisplayed = errorMessage.isDisplayed();
+        Assert.assertTrue(isErrorMessageDisplayed, "Validation if error message is displayed.");
+
+        final String actualErrorMessageDisplayed = driver.findElement(By.cssSelector("#login > div.row > div > div")).getText();
+        final String expectedErrorMessageDisplayed = "Invalid credentials!";
+        Assert.assertEquals(actualErrorMessageDisplayed, expectedErrorMessageDisplayed, "Validation if error message is indicating wrong credentials.");
+
+        final String actualNewPageLink = driver.getCurrentUrl();
+        final String expectedNewPageLink = "https://demo.placelab.com/";
+        Assert.assertEquals(actualNewPageLink, expectedNewPageLink, "Validation if user stayed on the log in page after attempt to login with false credentials");
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.close();
+    }
+}
